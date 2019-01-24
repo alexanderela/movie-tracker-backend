0946f35dfb3f3b1c2ca22decbee3a59f77638427
Alexander Ela <ela.alexander@gmail.com>
2019-01-24 15:24:11 -0700

    Add movie_id table to knex schema

--- migrations/20190124143811_initial.js
+++ migrations/20190124143811_initial.js
@@ -12,12 +12,13 @@ exports.up = function(knex, Promise) {
 
   	knex.schema.createTable('favorites', function(table) {
   		table.increments('id').primary();
+  		table.integer('movie_id');
+  		table.integer('user_id').unsigned();
   		table.string('title').notNullable();
   		table.text('poster_path').notNullable();
   		table.text('release_date').notNullable();
   		table.text('vote_average').notNullable();
   		table.text('overview').notNullable();
-  		table.integer('user_id').unsigned();
   		table.foreign('user_id').references('users.id')
 
   		table.timestamps(true, true);
