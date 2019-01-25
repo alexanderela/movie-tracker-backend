e1df741b64f1ec3dde42dd7fed4768854502ba88
Alexander Ela <ela.alexander@gmail.com>
2019-01-25 00:05:16 -0700

    Re-add CORS headers

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const cors = require('cors');
 const path = require('path');
 
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 3000);
