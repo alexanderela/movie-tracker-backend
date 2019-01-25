c0ecdfe88b41b90ee88a76bf069e0fdd79d87e4f
Alexander Ela <ela.alexander@gmail.com>
2019-01-24 23:53:47 -0700

    Install CORS aas dependency

--- server.js
+++ server.js
@@ -8,14 +8,7 @@ const cors = require('cors');
 const path = require('path');
 
 app.use(cors());
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
-}); 
 app.use(bodyParser.json());
-app.use(express.static('public'));
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'users';
