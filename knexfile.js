f656d99eb8b17501910baa4ef88d15b22ef90d3a
Alexander Ela <ela.alexander@gmail.com>
2019-01-24 21:27:49 -0700

    Add production environment to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -16,17 +16,13 @@ module.exports = {
     }
   },
 
-  // production: {
-  //   client: 'pg',
-  //   connection: process.env.DATABASE_URL + `?ssl=true`,
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     directory: './db/migrations'
-  //   },
-  //   useNullAsDefault: true
-  // }
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
 
 };
