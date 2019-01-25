caa971f8fb846b2c135a36a724db6d952b505412
Alexander Ela <ela.alexander@gmail.com>
2019-01-24 23:40:47 -0700

    Add cors headers to server.js

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 const app = express();
-const cors = require('cors');
+const cors = require('express-cors');
 const path = require('path');
 
 app.use(cors());
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+}); 
+
 app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 3000);
